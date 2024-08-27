7b7ea7b4564ba1d51286e0d5de89e6a4ce84ba09
ParkerJones567 <parker.jones@tum.de>
2024-08-27 20:50:47 +0200

    Add function to mark beginning of benchmark instruction count (no implementation yet)

--- sw/lib/terminate_benchmark.c
+++ sw/lib/terminate_benchmark.c
@@ -10,3 +10,8 @@ void benchmark_failure()
 {
     __asm__ volatile("jalr x0, 120(x0)");   //jump to 0x78 to signal failure caused by mismatched test output  Other interrupts are caught by 0x74 to signal a problem
 }
+
+void start_cycle_count()
+{
+    return; //TODO: Use this to signal the beginning of the cycle count
+}
